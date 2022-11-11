62a05cf13e99874e3affb9742021a1073bda84f4
김강년 <64246800+KangNyeonKim@users.noreply.github.com>
2022-11-11 20:47:15 +0900

    style: 줄 바꿈 변경

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,12 +12,7 @@ function App() {
 					Edit <code>src/App.tsx</code> and save to reload.
 				</p>
 				<Test />
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
+				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
 					Learn React
 				</a>
 			</header>
