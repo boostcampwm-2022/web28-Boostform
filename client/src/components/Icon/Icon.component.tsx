3db14dc63908f6320011ed5c9f91491dd1b0dab9
dohpark <dohpark1129@gmail.com>
2022-11-14 17:13:57 +0900

    feat: 아이콘 컴포넌트 추가

---
+++ client/src/components/Icon/Icon.component.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+import { ReactComponent as Plus } from "../../assets/Icon/plus.svg";
+import { ReactComponent as Kebab } from "../../assets/Icon/kebab.svg";
+
+interface ButtonProps {
+	size: string;
+}
+
+interface IconProps {
+	type: "plus" | "kebab";
+	size: string;
+}
+
+const Container = styled.div<ButtonProps>`
+	width: ${(props) => props.size};
+	height: ${(props) => props.size};
+`;
+
+function Icon({ type, size }: IconProps) {
+	return (
+		<Container size={size}>
+			{type === "plus" && <Plus />}
+			{type === "kebab" && <Kebab />}
+		</Container>
+	);
+}
+
+export default Icon;
