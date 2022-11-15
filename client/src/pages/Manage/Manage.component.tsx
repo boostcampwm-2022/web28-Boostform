8acb753052f5654e5eb3cf9c5bc40db91d170559
dohpark <dohpark1129@gmail.com>
2022-11-15 11:00:06 +0900

    feat: 설문지 관리 페이지 드랍다운 기능 추가

--- client/src/pages/Manage/Manage.component.tsx
+++ client/src/pages/Manage/Manage.component.tsx
@@ -29,7 +29,6 @@ const ListContainer = styled.div`
 	background-color: #ffffff;
 	border: 1px solid #afafaf;
 	border-radius: 9px;
-	overflow: hidden;
 `;
 
 const List = styled.ul`
@@ -40,6 +39,11 @@ const List = styled.ul`
 	background-color: #ffffff;
 	padding: 20px 20px;
 	font-size: 14px;
+
+	&:first-child {
+		border-top-left-radius: 9px;
+		border-top-right-radius: 9px;
+	}
 
 	&:hover {
 		background-color: #eaeaea;
@@ -72,6 +76,7 @@ const Category = styled.li`
 	width: 15%;
 `;
 const More = styled.li`
+	position: relative;
 	text-align: center;
 	width: 10%;
 `;
@@ -92,6 +97,28 @@ const Button = styled.button`
 	}
 `;
 
+const Dropdown = styled.ul`
+	position: absolute;
+	top: 40px;
+	right: -10px;
+	z-index: 1;
+	background-color: white;
+	padding: 10px 0;
+	border-radius: 9px;
+	border: 1px solid #afafaf;
+
+	li {
+		width: 180px;
+		padding: 10px;
+		text-align: left;
+		cursor: pointer;
+
+		&:hover {
+			background-color: #eaeaea;
+		}
+	}
+`;
+
 interface ListProps {
 	id: string;
 	title: string;
@@ -108,12 +135,18 @@ type ListArrayProps = ListProps[];
 function Manage() {
 	const [page, setPage] = useState(1);
 	const [list, setList] = useState<ListArrayProps>([]);
+	const [dropdownList, setDropdownList] = useState<boolean[]>([]);
 
 	useEffect(() => {
 		const source = axios.CancelToken.source();
 
-		axios(`http://localhost:8080/api/form/test/${page}`, { withCredentials: true, cancelToken: source.token })
-			.then((response) => setList((prev) => [...prev, ...response.data.form]))
+		axios(`http://localhost:8080/api/forms/test/${page}`, { withCredentials: true, cancelToken: source.token })
+			.then((response) => {
+				setList((prev) => [...prev, ...response.data.form]);
+
+				const falseArray = new Array(response.data.form.length).fill(false);
+				setDropdownList((prev) => [...prev, ...falseArray]);
+			})
 			.catch((e) => {
 				// eslint-disable-next-line no-console
 				if (e.message !== "cleanup") console.log(e);
@@ -124,6 +157,18 @@ function Manage() {
 
 	const onClickAddList: React.MouseEventHandler<HTMLButtonElement> = () => {
 		setPage((prev) => prev + 1);
+	};
+
+	const onClickOpenDropdown = (index: number) => {
+		setDropdownList((prev) => {
+			const value = prev[index];
+			const { length } = prev;
+
+			const curr = Array(length).fill(false);
+
+			curr[index] = !value;
+			return curr;
+		});
 	};
 
 	return (
@@ -141,7 +186,7 @@ function Manage() {
 			</HeaderContainer>
 			<ListContainer>
 				<>
-					{list.map(({ category, id, onBoard, response, title, updatedAt, acceptResponse }) => (
+					{list.map(({ category, id, onBoard, response, title, updatedAt, acceptResponse }, index) => (
 						<List key={id}>
 							<Title key={`${id}Title`}>{title}</Title>
 							<Status key={`${id}AcceptResponse`}>{acceptResponse ? "Open" : "Close"}</Status>
@@ -150,9 +195,17 @@ function Manage() {
 							<Share key={`${id}onBoard`}>{onBoard ? "On" : "Off"}</Share>
 							<Category key={`${id}Category`}>{category}</Category>
 							<More key={`${id}More`}>
-								<Button>
+								<Button type="button" onClick={() => onClickOpenDropdown(index)}>
 									<Icon type="kebab" size="16px" />
 								</Button>
+								{dropdownList[index] ? (
+									<Dropdown>
+										<li>이름 바꾸기</li>
+										<li>삭제</li>
+									</Dropdown>
+								) : (
+									""
+								)}
 							</More>
 						</List>
 					))}
