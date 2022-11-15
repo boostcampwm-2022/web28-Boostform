4ee785d55776468ea8f7844b8b845ebf875e7a92
dohpark <dohpark1129@gmail.com>
2022-11-15 16:04:10 +0900

    chore: Form.Controller에서 필요 없는 함수 삭제

--- server/src/Form/Form.Controller.ts
+++ server/src/Form/Form.Controller.ts
@@ -2,13 +2,6 @@ import { Response, NextFunction } from "express";
 import { CallFormListRequest, CreateNewFormRequest, UpdateFormRequest, DeleteFormRequest } from "./Form.Interface";
 import FormService from "./Form.Service";
 
-function toDateString(date: Date): string {
-  const yearString = date.getFullYear();
-  const monthString = date.getMonth() + 1 > 10 ? `${date.getMonth() + 1}` : `0${date.getMonth() + 1}`;
-  const dateString = date.getDate() > 10 ? `${date.getDate()}` : `0${date.getDate()}`;
-  return `${yearString}-${monthString}-${dateString}`;
-}
-
 class FormController {
   static createNewForm(req: CreateNewFormRequest, res: Response, next: NextFunction) {
     try {
