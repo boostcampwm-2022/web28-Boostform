86fac385726df473155f82adb97183b47f91b38b
김강년 <64246800+KangNyeonKim@users.noreply.github.com>
2022-11-15 17:17:37 +0900

    feat: 로그인 기능 구현

--- server/src/User/User.Controller.ts
+++ server/src/User/User.Controller.ts
@@ -1,18 +1,14 @@
+/* eslint-disable class-methods-use-this */
 import { Request, Response, NextFunction } from "express";
+import * as dotenv from "dotenv";
+import BadRequestException from "../Common/Exceptions/BadRequest.Exception";
 import userService from "./User.Service";
 
-type UserService = typeof userService;
-
+dotenv.config();
 class UserController {
-  #userService: UserService;
-
-  constructor(service: UserService) {
-    this.#userService = service;
-  }
-
   redirect(req: Request, res: Response, next: NextFunction) {
     try {
-      res.status(301).redirect(this.#userService.redirectURL);
+      res.status(301).redirect(userService.redirectURL);
     } catch (error) {
       next(error);
     }
@@ -21,12 +17,16 @@ class UserController {
   async login(req: Request, res: Response, next: NextFunction) {
     const { code } = req.query;
     if (!code || typeof code !== "string") {
-      throw new Error("invalid authorization code");
+      throw new BadRequestException();
     }
-    this.#userService
+    userService
       .login(code)
       .then((tokens) => {
-        res.status(200).cookie("accessToken", tokens.accessToken).cookie("refreshToken", tokens.refreshToken);
+        res
+          .status(200)
+          .cookie("accessToken", tokens.accessToken)
+          .cookie("refreshToken", tokens.refreshToken)
+          .redirect(process.env.ORIGIN_URL as string);
       })
       .catch((err) => {
         next(err);
@@ -34,4 +34,4 @@ class UserController {
   }
 }
 
-export default new UserController(userService);
+export default new UserController();
