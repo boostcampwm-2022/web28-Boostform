fa420c4b65ba128a7b92b1da2591d0c17d0bfd86
dohpark <dohpark1129@gmail.com>
2022-11-17 18:41:38 +0900

    refactor: 메인 페이지 컴포넌트, 스타일 분리

--- client/src/pages/Main/Main.component.tsx
+++ client/src/pages/Main/Main.component.tsx
@@ -1,64 +1,9 @@
 import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import styled from "styled-components";
 import Header from "components/Header/Header.component";
 import { AuthContext } from "contexts/authProvider";
 import Example from "assets/Images/Example.png";
-
-const Container = styled.section`
-  display: flex;
-  justify-content: space-between;
-  margin: 0 32px;
-  margin-top: 64px;
-  min-width: 1024px;
-`;
-
-const TextContainer = styled.div`
-  width: 50%;
-  margin-left: auto;
-`;
-
-const H1 = styled.h1`
-  font-size: 60px;
-  margin-right: auto;
-  user-select: none;
-  max-width: 600px;
-  line-height: 72px;
-`;
-
-const Text = styled.p`
-  margin-top: 36px;
-  font-size: 20px;
-  color: #5f6368;
-  line-height: 28px;
-  user-select: none;
-`;
-
-const ImageContainer = styled.div`
-  width: 50%;
-  margin-left: 36px;
-  user-select: none;
-`;
-
-const Button = styled.button`
-  background-color: #0066cc;
-  font-weight: 400;
-  color: #ffffff;
-  font-size: 15px;
-  padding: 8px 16px;
-  border: none;
-  font-size: 18px;
-  border-radius: 2px;
-  margin-top: 24px;
-  cursor: pointer;
-  user-select: none;
-`;
-
-const Image = styled.img`
-  width: 100%;
-  min-width: 400px;
-  max-width: 600px;
-`;
+import { Container, TextContainer, ImageContainer, H1, Image, Button, Text } from "./Main.style";
 
 function Main() {
   const { auth } = useContext(AuthContext);
