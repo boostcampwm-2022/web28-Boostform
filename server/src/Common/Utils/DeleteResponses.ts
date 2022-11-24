ed33a9f9704e0bc7c8fcbec1dd9c088f3b0ab357
beepbipbp <riskyjunk@gmail.com>
2022-11-24 16:57:38 +0900

    feat: 응답 삭제용 함수 추가

---
+++ server/src/Common/Utils/DeleteResponses.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+import FormResponse from "../../Response/Response.Model";
+
+dotenv.config();
+
+function connectDB() {
+  mongoose.connect(
+    `mongodb+srv://${process.env.MONGODB_ID}:${process.env.MONGODB_PASSWORD}@cluster0.a7vmgdw.mongodb.net/database0?`,
+    (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("mongoDB is connected...");
+      }
+    }
+  );
+}
+
+connectDB();
+
+(async () => {
+  try {
+    await FormResponse.deleteMany({ form_id: "637e2d988defe2cc0404ff2c" });
+    console.log("delete success");
+  } catch (err) {
+    console.log(err);
+  }
+})();
