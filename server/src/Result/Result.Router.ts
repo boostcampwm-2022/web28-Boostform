900d5992228e9b94be53d8024f27f29120861e67
김강년 <64246800+KangNyeonKim@users.noreply.github.com>
2022-11-30 14:27:25 +0900

    refactor: result 응답 형식에 설문지 제목 추가

--- server/src/Result/Result.Router.ts
+++ server/src/Result/Result.Router.ts
@@ -5,9 +5,10 @@ import { redisCli } from "../app";
 
 const router = express.Router();
 
-router.get("/:formId", resultController.formResult);
-
 router.get("/test", async (req: Request, res: Response) => {
   redisCli.set("key", "123");
 });
+
+router.get("/:formId", resultController.formResult);
+
 export default router;
