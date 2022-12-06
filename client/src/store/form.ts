650e7f8c52976027b80333b2df223b77215daae1
dohpark <dohpark1129@gmail.com>
2022-12-06 19:27:44 +0900

    feat: 폼 관련 상수 저장

---
+++ client/src/store/form.ts
@@ -0,0 +1,11 @@
+import { IconItem } from "components/molecules/Dropdown/IconDropdown/type";
+
+const CATEGORY_LIST = ["개발 및 학습", "취업 및 채용", "취미 및 여가", "기타"];
+
+const QUESTION_TYPE_LIST: IconItem[] = [
+  { text: "단수응답", icon: "checkbox", value: "checkbox" },
+  { text: "복수응답", icon: "multiple", value: "multiple" },
+  { text: "주관식", icon: "paragraph", value: "paragraph" },
+];
+
+export { CATEGORY_LIST, QUESTION_TYPE_LIST };
