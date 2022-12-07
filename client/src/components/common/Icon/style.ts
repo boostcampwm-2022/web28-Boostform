496df45a1b1f2370c9f59328110c1e717b7fe551
dohpark <dohpark1129@gmail.com>
2022-12-07 21:47:44 +0900

    design: 아이콘 크기 관련 css 수정

--- client/src/components/common/Icon/style.ts
+++ client/src/components/common/Icon/style.ts
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 
-const Container = styled.span`
+const Container = styled.span<{ size: string }>`
   display: inline-block;
+  height: ${({ size }) => size};
+  width: ${({ size }) => size};
 `;
 
 export default Container;
