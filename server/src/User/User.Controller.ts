82609cb89227b7a96fd091a2339f69fc7aa275ef
김강년 <64246800+KangNyeonKim@users.noreply.github.com>
2022-12-07 17:48:55 +0900

    fix: 로그아웃시 redirect하는 서버측 코드 삭제
    axios redirect 오류

--- server/src/User/User.Controller.ts
+++ server/src/User/User.Controller.ts
@@ -63,11 +63,7 @@ class UserController {
     userService
       .logout(userID)
       .then(() => {
-        res
-          .status(204)
-          .clearCookie("accessToken")
-          .clearCookie("refreshToken")
-          .redirect(`${process.env.ORIGIN_URL as string}/login`);
+        res.status(204).clearCookie("accessToken").clearCookie("refreshToken");
       })
       .catch((err) => {
         next(err);
