3696a2b7cec98414f3f56ce02ed9d06ea56bfabe
dohpark <dohpark1129@gmail.com>
2022-12-07 16:36:52 +0900

    design: theme에 색상 추가

--- client/src/styles/theme.ts
+++ client/src/styles/theme.ts
@@ -11,11 +11,13 @@ const colors = {
   grey7: "#5f6368",
   grey8: "#555555",
   grey9: "#444444",
+  blue0: "#cae1ff",
   blue1: "#1a73e8",
   blue2: "#0066cc",
   blue3: "#3c64b1",
   blue4: "#3e57cb",
   blue5: "#284b8f",
+  red0: "#ffc9c9",
   red1: "#d93025",
 };
 
