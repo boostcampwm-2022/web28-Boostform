4b2079adfc1d015ead2eceb41b68fafece29a61e
beepbipbp <riskyjunk@gmail.com>
2022-12-08 20:46:07 +0900

    chore: ResponseUpdateScheduler.ts에 로직 설명 주석 추가

--- server/src/Response/Utils/ResponseUpdateScheduler.ts
+++ server/src/Response/Utils/ResponseUpdateScheduler.ts
@@ -22,6 +22,8 @@ class ResponseUpdateScheduler extends Scheduler {
 
               FormResponse.exists({ _id: responseId }).then(async (result) => {
                 if (result) {
+                  // 응답지가 DB에 존재하는 경우
+                  // findOneAndUpdate 진행
                   const answerList = JSON.parse(answerListString);
                   await FormResponse.findOneAndUpdate(
                     { _id: responseId },
@@ -29,9 +31,15 @@ class ResponseUpdateScheduler extends Scheduler {
                   ).exec();
 
                   if (answerListString === (await redisCli.hGet("response_update", responseId))) {
+                    // 해당 responseId로 새로운 수정 사항이 생겼는지 확인
+                    // 만약 새로운 수정 사항이 없다면 redis에서 삭제
+                    // 만약 새로운 수정 사항이 있다면 다음 작업에서 처리하도록 남겨둠
                     await redisCli.hDel("response_update", responseId);
                   }
                 } else {
+                  // 응답지가 아직 DB에 존재하지 않는 경우
+                  // findOneAndUpdate 메소드를 적용할 수 없으므로
+                  // 이번 작업에서는 처리하지 않고, 다음 작업으로 미룸
                   await redisCli.hSet("response_update", responseId, answerListString);
                 }
                 res(true);
