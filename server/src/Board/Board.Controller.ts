53554959091bfc076d3734555bc443d7901b6856
unknown <gotoerror00111011@gmail.com>
2022-12-08 15:45:23 +0900

    feat: 게시판 검색 시 최대 페이지 수 캐싱 추가

--- server/src/Board/Board.Controller.ts
+++ server/src/Board/Board.Controller.ts
@@ -7,21 +7,29 @@ import { filterByObjectKeys } from "./Board.Utils";
 
 class BoardController {
   static async getFormList(req: Request, res: Response, next: NextFunction) {
-    const cacheKey = `board:${JSON.stringify(req.query)}`;
     const searchQuery = filterByObjectKeys(req.query, searchKeyList);
     const sortQuery = filterByObjectKeys(req.query, sortKeyList);
     const pageNum = req.query.page ? Number(req.query.page) : 1;
 
-    let searchResult = JSON.parse(await redisCli.get(cacheKey));
+    const cacheKey = `board:${JSON.stringify(searchQuery)},${JSON.stringify(sortQuery)}`;
+    const pageCacheKey = `${cacheKey},{page:${pageNum}}`;
+    const countCacheKey = `${cacheKey},{count}`;
+
+    let searchResult = JSON.parse(await redisCli.get(pageCacheKey));
     if (!searchResult) {
       searchResult = await BoardService.searchByQuery(searchQuery, sortQuery, pageNum);
       if (!req.query.title) {
-        redisCli.set(cacheKey, JSON.stringify(searchResult));
-        redisCli.expire(cacheKey, 120);
+        redisCli.set(pageCacheKey, JSON.stringify(searchResult));
+        redisCli.expire(pageCacheKey, 120);
       }
     }
 
-    const lastPage = await BoardService.countByQuery(searchQuery);
+    let lastPage = JSON.parse(await redisCli.get(countCacheKey));
+    if (!lastPage) {
+      lastPage = await BoardService.countByQuery(searchQuery);
+      redisCli.set(countCacheKey, JSON.stringify(lastPage));
+      redisCli.expire(countCacheKey, 600);
+    }
 
     res.status(200).send({ form: searchResult, lastPage });
   }
