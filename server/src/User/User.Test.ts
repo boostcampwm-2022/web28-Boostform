c1e438210dfb4b089194dda71a3038222314f177
김강년 <64246800+KangNyeonKim@users.noreply.github.com>
2022-12-12 23:30:23 +0900

    style: 코드 스타일 수정

--- server/src/User/User.Test.ts
+++ server/src/User/User.Test.ts
@@ -8,7 +8,16 @@ import app from "../app";
 
 dotenv.config();
 
-beforeAll(async () => {
+const testUser = {
+  name: "testUser",
+  id: 58,
+  infAccessToken:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NTgsImlhdCI6MTY3MDgyODE2MH0.vQrK4E6RbQfPxpTt0cjBmhYVtlihWJelKSdssm2W45E",
+  infRefreshToken:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NTgsImlhdCI6MTY3MDgyODE2NH0.07bukMgpAcfDik8Umlg2UtTvVKmWFotf3AHy_legMWs",
+};
+
+const testUserRefreshToken = beforeAll(async () => {
   const myDataSource = new DataSource({
     type: "mysql",
     host: process.env.TYPEORM_HOST || "",
@@ -25,19 +34,13 @@ beforeAll(async () => {
 
 // 본인 유저 정보
 describe("GET api/users", () => {
-  const testUserName = "testUser";
-  const testUserId = 58;
-  const testUserAccessToken =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NTgsImlhdCI6MTY3MDgyODE2MH0.vQrK4E6RbQfPxpTt0cjBmhYVtlihWJelKSdssm2W45E";
-  const testUserRefreshToken =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NTgsImlhdCI6MTY3MDgyODE2NH0.07bukMgpAcfDik8Umlg2UtTvVKmWFotf3AHy_legMWs";
   test("로그인한 유저의 경우 유저 정보 반환", (done) => {
     request(app)
       .get("/api/users")
-      .set("Cookie", [`accessToken=${testUserAccessToken}`, `refreshToken=${testUserRefreshToken}`])
+      .set("Cookie", [`accessToken=${testUser.infAccessToken}`, `refreshToken=${testUser.infRefreshToken}`])
       .expect(200)
       .then((res) => {
-        expect(res.body).toEqual({ userID: testUserId, userName: testUserName });
+        expect(res.body).toEqual({ userID: testUser.id, userName: testUser.name });
         done();
       })
       .catch((err) => {
