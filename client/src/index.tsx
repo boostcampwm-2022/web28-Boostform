05e5f671ce23828a9466b3be6b6b5eaadf5b52fe
dohpark <dohpark1129@gmail.com>
2022-12-15 18:21:14 +0900

    chore: reportWebVitals 삭제

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -8,7 +8,6 @@ import router from "router";
 import GlobalStyle from "styles/GlobalStyle";
 import theme from "styles/theme";
 import { AuthProvider } from "contexts/authProvider";
-import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 const queryClient = new QueryClient();
@@ -26,8 +25,3 @@ root.render(
     </ThemeProvider>
   </>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
